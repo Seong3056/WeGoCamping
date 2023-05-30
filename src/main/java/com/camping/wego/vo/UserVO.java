712767811666e883c587529cae86856f071c9ce2
Seong3056 <aktnf207@naver.com>
2023-05-30 10:33:46 +0900

    vo table 구현

--- src/main/java/com/camping/wego/vo/UserVO.java
+++ src/main/java/com/camping/wego/vo/UserVO.java
@@ -1,5 +1,15 @@
 package com.camping.wego.vo;
-
+/*
+CREATE TABLE User (
+	id	VARCHAR(50)	NOT NULL,
+	pw	VARCHAR(50)	NOT NULL,
+	name	VARCHAR(20)	NOT NULL,
+	phone	VARCHAR(10)	NOT NULL,
+	email	VARCHAR(50)	NULL,
+	address	VARCHAR(100)	NOT NULL,
+	gender	VARCHAR(5)	NULL
+);
+ */
 public class UserVO {
 
 }
