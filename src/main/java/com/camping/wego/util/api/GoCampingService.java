0886563131021aa3cb0db8102ec9ca8eeb235b18
Seong <aktnf207@naver.com>
2023-05-30 22:37:55 +0900

    서버정상동작

--- src/main/java/com/camping/wego/util/api/GoCampingService.java
+++ src/main/java/com/camping/wego/util/api/GoCampingService.java
@@ -1,28 +1,28 @@
-package com.camping.wego.util.api;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import com.camping.wego.vo.PageVO;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Component
-@Slf4j
-public class GoCampingService {
-	
-	@Value("${GoCamping.serviceKey}")
-	private String serviceKey;
-	
-	public String getBasedListUrl(PageVO paging) {
-		
-		String condition = "/basedList";
-		String mobileOS = "ETC";
-		String mobileApp = "WeGoCamping";
-		
-		String requestUrl = String.format("https://apis.data.go.kr/B551011/GoCamping%s?numOfRows=%d&pageNo=%d&MobileOS=%s&MobileApp=%s&serviceKey=%s&_type=json", paging.getCpp(), paging.getPageNum(), condition, mobileOS, mobileApp, serviceKey);
-		
-		return requestUrl;
-	}
-
-}
+//package com.camping.wego.util.api;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import com.camping.wego.vo.PageVO;
+//
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Component
+//@Slf4j
+//public class GoCampingService {
+//	
+//	@Value("${GoCamping.serviceKey}")
+//	private String serviceKey;
+//	
+//	public String getBasedListUrl(PageVO paging) {
+//		
+//		String condition = "/basedList";
+//		String mobileOS = "ETC";
+//		String mobileApp = "WeGoCamping";
+//		
+//		String requestUrl = String.format("https://apis.data.go.kr/B551011/GoCamping%s?numOfRows=%d&pageNo=%d&MobileOS=%s&MobileApp=%s&serviceKey=%s&_type=json", paging.getCpp(), paging.getPageNum(), condition, mobileOS, mobileApp, serviceKey);
+//		
+//		return requestUrl;
+//	}
+//
+//}
