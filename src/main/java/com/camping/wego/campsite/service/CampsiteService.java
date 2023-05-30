c8e62533d5c058a70c316b7d8c41451a9be75bed
flatcalm <a_abnc@naver.com>
2023-05-30 19:42:22 +0900

    캠핑장 JSON 데이터 파일 DB 저장페이지 추가

---
+++ src/main/java/com/camping/wego/campsite/service/CampsiteService.java
@@ -0,0 +1,57 @@
+package com.camping.wego.campsite.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.camping.wego.campsite.mapper.ICampsiteMapper;
+import com.camping.wego.vo.CampsiteVO;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class CampsiteService implements ICampsiteService {
+
+	@Autowired
+	private ICampsiteMapper mapper;
+	
+	@Override
+	public void readOpenStoresFromJson(int num) {
+        List<CampsiteVO> campsiteList = new ArrayList<>();
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File("C:/work/campsite info_" + num + "_1000.json"));
+            JsonNode responseNode = rootNode.get("response");
+            JsonNode bodyNode = responseNode.get("body");
+            JsonNode itemNode = bodyNode.get("items").get("item");
+
+            if(itemNode.isArray()) {
+                for(JsonNode node : itemNode) {
+                    CampsiteVO campsite = objectMapper.convertValue(node, CampsiteVO.class);
+                    campsiteList.add(campsite);
+                }
+            }
+            
+            campsiteList.stream().forEach(vo -> {
+            	if(vo.getTel().equals("")) {
+            		vo.setTel(null);
+            	}
+            });
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(CampsiteVO vo : campsiteList) {
+            mapper.insert(vo);
+        }
+	}
+
+}
