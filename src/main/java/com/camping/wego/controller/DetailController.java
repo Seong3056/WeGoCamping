b03e07a43c0da82bf3daac292476cbd9e6905bf6
flatcalm <a_abnc@naver.com>
2023-06-01 20:02:21 +0900

    지도 좌표값 수정 및 약간 수정

--- src/main/java/com/camping/wego/controller/DetailController.java
+++ src/main/java/com/camping/wego/controller/DetailController.java
@@ -2,15 +2,25 @@ package com.camping.wego.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/detail")
+@Slf4j
 public class DetailController {
 	
 	@GetMapping("")
 	public String main() {
 		return "/detail/detail";
+	}
+	
+	@GetMapping("/{cno}")
+	public String detail(@PathVariable int cno) {
+		log.info("{}번 캠핑장", cno);
+		return "detail/detail";
 	}
 
 }
