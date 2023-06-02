fe8ec6e1edf589b9803af1d5d9e0415d8e907aa1
ny <ny567@naver.com>
2023-06-02 11:20:29 +0900

    230602 메인 오늘의 캠핑장 수정

--- src/main/java/com/camping/wego/controller/DetailController.java
+++ src/main/java/com/camping/wego/controller/DetailController.java
@@ -6,13 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-<<<<<<< HEAD
 import com.camping.wego.campsite.service.ICampsiteService;
-import com.google.protobuf.Service;
 
-=======
->>>>>>> faf155443d14c93056706a7c8d31399d1bdce2e4
+
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -27,7 +23,7 @@ public class DetailController {
 	public String main() {
 		return "/detail/detail";
 	}
-<<<<<<< HEAD
+
 	@GetMapping("/{cno}")
 	public String detail(@PathVariable int cno, Model model) {
 		log.info(cno+"의 상세보기 페이지 이동");
@@ -35,15 +31,6 @@ public class DetailController {
 		model.addAttribute("camp",service.info(cno));
 		return "/detail/detail";
 	}
-
-=======
-	
-	@GetMapping("/{cno}")
-	public String detail(@PathVariable int cno) {
-		log.info("{}번 캠핑장", cno);
-		return "detail/detail";
-	}
->>>>>>> faf155443d14c93056706a7c8d31399d1bdce2e4
 
 }
 
